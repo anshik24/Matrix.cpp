cb475e8a9fd5233ef04c2288fcd12e1eaabb89a0
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-05-10 22:21:30 +0530

    Create transposeMatrix.cpp

---
+++ transposeMatrix.cpp
@@ -0,0 +1,16 @@
+class Solution
+{   
+    public:  
+    //Function to find transpose of a matrix.
+    void transpose(vector<vector<int> >& matrix, int n)
+    { 
+        // code here 
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+              }
+              }
+    }
+};
